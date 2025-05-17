abf821242f3c9c137c7d40718d78aa309539b45f
Agustin800 <aguslahalo@gmail.com>
2025-05-17 12:06:35 -0300

    retorno la mejor key

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -8,4 +8,4 @@ def best_score(a_dictionary):
           if a_dictionary[key] > best_value:
                 best_value = a_dictionary[key]
                 best_key = key
-    return max
+    return best_key
