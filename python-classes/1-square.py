4473ba43eddedff0ffc67004cf593208f09aa07c
Agustin800 <aguslahalo@gmail.com>
2025-05-21 12:45:51 -0300

    documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,5 +5,14 @@ Este módulo define la clase Square que representa un cuadrado con un tamaño da
 
 
 class  Square:
+    """
+    Clase que define un cuadrado con un atributo privado de tamaño.
+    """
     def __init__(self, size):
+        """
+        Inicializa una nueva instancia de Square con un tamaño dado.
+
+        Args:
+            size: El tamaño del lado del cuadrado.
+        """
         self.__size = size
