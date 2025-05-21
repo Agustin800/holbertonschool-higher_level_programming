2570d41212c3b39d10f3ebb5e5bedeab35f34e1e
Agustin800 <aguslahalo@gmail.com>
2025-05-21 13:05:00 -0300

    area of a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+
+"""
+
+
+class Square:
+    """
+    Clase que define un cuadrado con un atributo privado de tamaño.
+    """
+    def __init__(self, size=0):
+        """
+        Inicializa una nueva instancia de Square.
+
+        Args:
+            size (int): El tamaño del lado del cuadrado (por defecto 0).
+
+        Raises:
+            TypeError: Si size no es un entero.
+            ValueError: Si size es menor que 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """
+        Devuelve el área del cuadrado actual.
+        """
+        return self.__size ** 2
