26427f64de4bff43c7653ec70820446c7eb30137
Agustin800 <aguslahalo@gmail.com>
2025-05-24 10:35:53 -0300

    pycodestyle

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -56,7 +56,10 @@ class Rectangle:
         '''Imprime el rectangulo'''
         if self.__height == 0 or self.__width == 0:
             return ""
-        lines = [str(self.print_symbol) * self.__width for _ in range(self.__height)]
+        lines = [
+            str(self.print_symbol) * self.__width
+            for _ in range(self.__height)
+        ]
         return "\n".join(lines)
 
     def __repr__(self):
