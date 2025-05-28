745e3f93bca3f77220c3147b66add2c51dfa9734
Agustin800 <aguslahalo@gmail.com>
2025-05-28 16:35:26 -0300

    Clase mylist que toma los metodos publicos de list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Clase MyList que hereda de list'''
+
+
+class Mylist(list):
+    '''Subclase de list con un método para imprimir ordenado'''
+
+    def print_sorted(self):
+        '''Imprime la lista ordenada sin modificar la original'''
+        print(sorted(self))
