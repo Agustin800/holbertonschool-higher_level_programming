98799395db7c6316ce4028196f43fe8e2a53a4ba
Agustin800 <aguslahalo@gmail.com>
2025-05-29 15:25:21 -0300

    type() pDocumentacion

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+'''Modulo'''
+
+
 def is_same_class(obj, a_class):
+    '''Revisa con type() que obj sea igual a la instancia en a_class'''
+
     if type(obj) == a_class:
         return True
     else:
