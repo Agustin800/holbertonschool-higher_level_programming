76a676045384442fded9e3a02ad721f8b5568707
Agustin800 <aguslahalo@gmail.com>
2025-05-29 15:28:04 -0300

    cambio == por is

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,7 +5,7 @@
 def is_same_class(obj, a_class):
     '''Revisa con type() que obj sea igual a la instancia en a_class'''
 
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     else:
         return False
