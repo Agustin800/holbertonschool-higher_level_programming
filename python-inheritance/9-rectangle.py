192f82d6f570b731101649aa4f1702c4ffdca862
Agustin800 <aguslahalo@gmail.com>
2025-05-30 14:29:46 -0300

    funcion area y str() aparece un output personalizado

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Modulo'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''Clase hija de BaseGeometry'''
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        '''Devuelve el area del rectangulo'''
+        return self.__width * self.__height
+
+    def __str__(self):
+        '''Imprime el rectangulo'''
+        return f"[Rectangle] {self.__width}/{self.__height}"
