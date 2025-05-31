d43c1e8dc2f8a8932ebd64a04e9d37e704724a9d
Agustin800 <aguslahalo@gmail.com>
2025-05-31 13:52:52 -0300

    metodo area

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -24,3 +24,6 @@ class Square(Rectangle):
     def __init__(self, size):
         self.__size = size
         self.integer_validator("size", size)
+
+    def area(self):
+        return self.__size ** 2
