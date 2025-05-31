ec2967eecda43e10be9726d32773f22557fac092
Agustin800 <aguslahalo@gmail.com>
2025-05-31 14:04:44 -0300

    Elimino el codigo e importo el modulo directamente

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,23 +1,6 @@
 #!/usr/bin/python3
 '''Modulo'''
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    '''Clase hija de BaseGeometry'''
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        '''Devuelve el area del rectangulo'''
-        return self.__width * self.__height
-
-    def __str__(self):
-        '''Imprime el rectangulo'''
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     '''Clase cuadrado'''
