129381edb11cb98991381d49b94a323bf9af0491
Agustin800 <aguslahalo@gmail.com>
2025-05-31 14:01:31 -0300

    llamada a la super clase y cambio en el orden del init en square

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -22,8 +22,9 @@ class Rectangle(BaseGeometry):
 class Square(Rectangle):
     '''Clase cuadrado'''
     def __init__(self, size):
-        self.__size = size
         self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
 
     def area(self):
         return self.__size ** 2
