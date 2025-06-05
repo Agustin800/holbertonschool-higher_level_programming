ae8849c0b580dcd53173985ccfb2e744c814193d
Agustin800 <aguslahalo@gmail.com>
2025-06-05 16:42:00 -0300

    utf-8 correctamente

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     '''Funcion'''
-    with open(filename, "w", encoding="utf-81")as f:
+    with open(filename, "w", encoding="utf-8")as f:
         return f.write(text)
