e08f81b6f3fa214293b18aa02a59e0492f1398d4
Agustin800 <aguslahalo@gmail.com>
2025-06-07 01:28:26 -0300

    escribir un obj en json dentro de un archivo

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Modulo'''
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    '''Funcion para escribir un obj en un archivo en formato json'''
+    with open(filename, "w") as f:
+        json.dumps(my_obj, f)
