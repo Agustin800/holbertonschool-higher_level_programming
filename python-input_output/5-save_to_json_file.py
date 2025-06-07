6977c9560b47285f01f2319e6d035b081b3546f2
Agustin800 <aguslahalo@gmail.com>
2025-06-07 01:30:19 -0300

    saco el return y cambio a json.dump

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     '''Funcion para escribir un obj en un archivo en formato json'''
     with open(filename, "w") as f:
-       return json.dumps(my_obj, f)
+        json.dump(my_obj, f)
