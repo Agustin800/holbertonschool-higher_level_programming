4f5d7db6ee665e695732df7272873b5046e96395
Agustin800 <aguslahalo@gmail.com>
2025-06-08 03:50:25 -0300

    triangulo de pascal

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''Modulo'''
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        if i ==0:
+            triangle.append([1])
+        else:
+            prev_row = triangle.append([-1])
+            new_row = [1]
+
+            for j in range(len(prev_row) - 1):
+                new_row.append(prev_row[j] + prev_row[j - 1])
+
+            new_row.append(1)
+            triangle.append(new_row)
+
+    return triangle
+ 
