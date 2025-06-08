363bf37cdb1baf75779f3b7d4040177accce95c3
Agustin800 <aguslahalo@gmail.com>
2025-06-08 03:36:12 -0300

    pycodestyle2

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -10,7 +10,10 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+        if (
+            isinstance(attrs, list)
+            and all(isinstance(attr, str) for attr in attrs)
+        ):
             return {
                 key: getattr(self, key)
                 for key in attrs
