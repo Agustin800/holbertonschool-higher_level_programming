a18d1f45081bf7baedf2b67bbb99700e67e4cb4e
Agustin800 <aguslahalo@gmail.com>
2025-06-08 03:54:11 -0300

    saco el append para prev_row

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -13,11 +13,11 @@ def pascal_triangle(n):
         if i ==0:
             triangle.append([1])
         else:
-            prev_row = triangle.append([-1])
+            prev_row = triangle[-1]
             new_row = [1]
 
             for j in range(len(prev_row) - 1):
-                new_row.append(prev_row[j] + prev_row[j - 1])
+                new_row.append(prev_row[j] + prev_row[j + 1])
 
             new_row.append(1)
             triangle.append(new_row)
