2e5c450dd5f3de99d5bccc0a6e8a591e2e24ccb6
Agustin800 <aguslahalo@gmail.com>
2025-06-11 18:46:11 -0300

    primera funcion de la api

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+'''Modulo'''
 import requests
+import csv
 
 
 def fetch_and_print_posts():
-    requests.get()
+    url = 'https://jsonplaceholder.typicode.com'
+    response = requests.get(url)
+
+    print(f'Status Code: {response.status_codes}')
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+    
