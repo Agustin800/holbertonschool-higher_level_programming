e23f971028db0747229c73556fd3f4f2bf7584fb
Agustin800 <aguslahalo@gmail.com>
2025-06-27 03:12:42 +0000

    unique id sin default porque no es compatible con auto_increment

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
 -- a script that creates the table unique_id on your MySQL server.
 
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256)
 );
