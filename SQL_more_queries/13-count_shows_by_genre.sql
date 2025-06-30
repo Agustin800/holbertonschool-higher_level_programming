2f74f2bae33af7cb7952ff89be8108ba3763e69e
Agustin800 <aguslahalo@gmail.com>
2025-06-30 17:52:04 +0000

    Number of shows by genre

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+
+SELECT genre COUNT(*) AS number_of_shows
+FROM shows
+GROUP BY genre
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;
