4833014624c974872f857f412105d0f30f37de6f
Agustin800 <aguslahalo@gmail.com>
2025-06-30 16:33:43 +0000

    Genre ID by show

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+
+SELECT tv_show.title, tv_show_generes.genere_id
+FROM tv_show
+JOIN tv_show_generes ON tv_show.id = tv_show_generes.genere_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
