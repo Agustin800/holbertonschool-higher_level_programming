5c01074ed77366df1c029ddf1155a5f746ea3715
Agustin800 <aguslahalo@gmail.com>
2025-07-06 00:41:11 +0000

    FIX:Filter states, arreglado completo

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -3,22 +3,24 @@
 import MySQLdb
 import sys
 
+import MySQLdb
+import sys
+
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    mysql_user = sys.argv[1]
+    mysql_pass = sys.argv[2]
+    db_name = sys.argv[3]
 
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database
+        user=mysql_user,
+        passwd=mysql_pass,
+        db=db_name
     )
 
     cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%'ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
 
     states = cursor.fetchall()
 
